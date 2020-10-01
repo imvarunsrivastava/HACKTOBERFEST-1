cd8db215c2179cc4473e064189441cebed897470
rockingdeep <deeppiyushvora@gmail.com>
2020-10-01 10:58:30 +0530

    adding queues in C++

--- Hackerrank solutions in java/Plus-Minus.java
+++ Hackerrank solutions in java/Plus-Minus.java
@@ -1,57 +1,33 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
-public class Solution {
+public class PlusMinus 
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		double plus=0,minus=0,zero=0;
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+			if(arr[i]>0)
+			{
+				plus++;
+			}
+			if(arr[i]<0)
+			{
+				minus++;
+			}
+			if(arr[i]==0)
+			{
+				zero++;
+			}
+		}
+		sc.close();
+		System.out.printf("%.6f \n", plus/n);
+		System.out.printf("%.6f \n", minus/n);
+		System.out.printf("%.6f \n", zero/n);
+	}
 
-    // Complete the plusMinus function below.
-    static void plusMinus(int[] arr) {
-double count1=0,count2=0,count3=0;
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]>0)
-            {
-                count1++;
-            }
-            else if(arr[i]<0)
-            {
-                count2++;
-            }
-            else
-                count3++;
-        }
-        
-        double pos=(double)(count1/arr.length);
-        double neg=(double)(count2/arr.length);
-        double zero=(double)(count3/arr.length);
-        System.out.println(pos);
-        System.out.println(neg);
-        System.out.println(zero);
-
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] arr = new int[n];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
-
-        plusMinus(arr);
-
-        scanner.close();
-    }
 }
